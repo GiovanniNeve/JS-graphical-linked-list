e9a5e8abcd996099bf2ff6d8371388006853916a
thomas zambelli <thomasz01@outlook.itname>
2020-05-14 20:59:00 +0200

    Fix

---
+++ canvasMain.js
@@ -0,0 +1,44 @@
+/* -------------------------------------------------- Main -------------------------------------------------- */
+
+var list = new linkedList(); /* Create linled list class object */
+
+/* ---------- Function who draws the list in the cancvas (Button) ---------- */
+function drawNode() {
+    list.clearScreen();
+    list.drawList(list.node); /* Call the drawList function */
+}
+
+/* ---------- Insert random numbers in the list (Button) ---------- */
+function randomInsert() {
+    for (var i = 0; i < 29; i++) {
+        var data = Math.floor(Math.random() * 200) + 1;
+        list.insertNode(list.node, data);
+        arraylength += 1; /* Get array length */
+        if (Number(maxNumber) < Number(data)) {
+            maxNumber = data; /* Get max number */
+        }
+    }
+    console.log("Random insert done");
+}
+
+/* ---------- Sort the list with the selection sort alorithm (Button) ---------- */
+function selectionSort() {
+    list.selectionSort(list.node);
+}
+
+/* ---------- Set animation delay (Button) ---------- */
+function setDelay() {
+    delayTime = document.querySelector(".delayButton > select").value;
+}
+
+/* ---------- Insertion sort (Button) ---------- */
+function insertionSort() {
+    list.insertionSort(list.node);
+    console.log("Insertion sort done");
+}
+
+/* ---------- Merge sort (Button) ---------- */
+function mergeSort() {
+    list.mergeSort(list.node);
+    console.log("Merge sort done");
+}
